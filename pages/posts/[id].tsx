7aefdbef8e234c00e26994139eb3cbe1d505d4b2
mongolyy <mongolyy@gmail.com>
2020-12-08 18:21:01 +0900

    typescriptを導入

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -3,8 +3,9 @@ import { getAllPostIds, getPostData } from '../../lib/posts'
 import Head from "next/head";
 import Date from "../../components/date";
 import utilStyles from '../../styles/utils.module.css'
+import { GetStaticProps, GetStaticPaths } from "next";
 
-export default function Post({ postData }) {
+const Post = ({ postData }) => {
   return (
     <Layout>
       <Head>
@@ -21,7 +22,9 @@ export default function Post({ postData }) {
   )
 }
 
-export async function getStaticPaths() {
+export default Post
+
+export const getStaticPaths: GetStaticPaths = async () => {
   const paths = getAllPostIds()
   return{
     paths,
@@ -29,7 +32,7 @@ export async function getStaticPaths() {
   }
 }
 
-export async function getStaticProps({ params }) {
+export const getStaticProps: GetStaticProps = async ({ params }) => {
   const postData = await getPostData(params.id)
   return {
     props: {
